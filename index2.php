ccf84ac200c9a4f9e206fdfe2ec5bd5a1a6d2834
ASCC Developer <communications@austinstone.org>
2011-10-21 16:02:41 -0500

    index file

---
+++ index2.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>AS Worship</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+    <link href='css/jplayer_style.css' rel='stylesheet' type='text/css' />
+    <link href='css/thickbox.css' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">	
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script> 
+
+	<script type="text/javascript" src="http://use.typekit.com/dvt1bdb.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+    <script src='js/jquery.min.js' type='text/javascript'></script>
+    <script src='js/scripts.js' type='text/javascript'></script>
+    <script src='js/thickbox-compressed.js' type='text/javascript'></script>
+    <script src='js/jquery.jplayer.min.js' type='text/javascript' ></script>
+    <script type='text/javascript'>
+    $(document).ready( function(){
+        $("#jquery_jplayer_1").jPlayer({
+                    ready: function () {
+                            $(this).jPlayer("setMedia", {
+                                    mp3: "/music/god_undefeatable.mp3",
+                            });
+                    },
+                    play: function() { // To avoid both jPlayers playing together.
+                            $(this).jPlayer("pauseOthers");
+                    },
+                    swfPath: "/",
+                    supplied: "mp3",
+                    cssSelectorAncestor: "#jp_container_1",
+                    wmode: "window"
+            });
+        });
+    </script>	
+    
+</head>
+
+<body id="">
+
+	<div class="head">
+		
+		<div class="container">
+			<a href="/"><div class="logo">Austin Stone Worship</div></a>
+			
+			<h1 class="cap">The first live record from The Austin Stone releases nationally on Tuesday, December 6. The eleven songs were tracked in downtown Austin, Texas, at The Austin Stone in the Spring of 2011, featuring songs from worship leaders Aaron Ivey, Jimmy McNeal, and Andy Melvin.</h1>
+					
+		</div> <!-- /container -->
+		
+	</div><!--/.head-->
+	
+	<div class="divide">&nbsp;</div>
+	
+		<!-- TABS -->
+		<div id="page-wrap">
+    		<div id="gallery">
+    			<ul class="nav_tab">
+            		<li class="nav-one"><a href="#t1" class="current">Track 01</a></li>
+            		<li class="nav-two"><a href="#t2">Track 02</a></li>
+            		<li class="nav-three"><a href="#t3">Track 03</a></li>
+        		</ul>
+    		<div class="list-wrap">
+    		 	<ul id="t1">
+    		 		<div class="container">
+						  <!-- //First player -->
+						<div id="jquery_jplayer_1" class="jp-jplayer"></div>
+						
+						<div id="jp_container_1" class="jp-audio">
+					        <div class="jp-type-single">
+				                <div class="jp-gui jp-interface">
+				                        <h3>TRACK</h3>
+				                        <h2 class="song_title"><span class="dark_gold">01</span> &nbsp;/&nbsp; GOD UNDEFEATABLE</h2>
+				                        <div class="jp-progress">
+				                                <div class="jp-seek-bar">
+				                                        <div class="jp-play-bar"></div>
+				                                </div>
+				                        </div>
+				                        <div class="jp-controls-holder">
+				                            <ul class="jp-controls">
+				                                    <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+				                                    <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+				                                    <li>&nbsp;/&nbsp;</li>
+				                                    <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+				                            </ul>
+				                        </div>
+				                        <div class="jp-time-holder">
+				                                <div class="jp-current-time"></div>
+				                                &ndash;
+				                                <div class="jp-duration"></div>
+				                        </div>
+				                        <h4 class="download"><a href="#TB_inline?height=155&width=300&inlineId=email_signup" class="thickbox">Free Download</a></h4>
+				                        <!--<form name="input" action="/" method="post">
+				                            Email Address: <input type="text" name="email" />
+				                            <input type="hidden" name="track" value="God Undefeatable" />
+				                            <input type="submit" value="Submit" />
+				                        </form>--> 
+				                </div>
+				
+				                <div class="jp-no-solution">
+				                        <span>Update Required</span>
+				                        To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+				                </div>
+					        </div>
+						</div><!-- //End first player -->
+						
+						<div class="bumpbump">&nbsp;</div>
+						
+						<div class="onecol"><h1>Theology &amp; Resources</h1></div>
+						
+						<div class="threecol top_border">
+							<img src="images/theo_t1.png"/>
+							<h2>Song Theology</h2>
+						</div>
+						
+						<div class="threecol top_border">
+							<img src="images/view_chord_t1.png"/>
+							<h2>View Chord Chart</h2>
+						</div>
+						
+						<div class="threecol top_border">
+							<img src="images/download_chord_t1.png"/>
+							<h2>Download Chord Chart</h2>
+						</div>
+						
+						<div class="bumpbump">&nbsp;</div>
+						
+						<div class="onecol"><h1>Teaching Videos</h1></div>
+						
+						<div class="fourcol vid1_t1 top_border">
+							<a href=""><img src="images/story_t1.jpg"/></a>
+							<h2>Song Story</h2>						
+						</div>
+						
+						<div class="fourcol top_border">
+							<a href=""><img src="images/piano_t1.jpg"/></a>
+							<h2>Piano</h2>						
+						</div>
+						
+						<div class="fourcol vid3_t1 top_border">
+							<a href=""><img src="images/guitar_t1.jpg"/></a>
+							<h2>Drums</h2>						
+						</div>
+						
+						<div class="fourcol vid4_t1 top_border">
+							<a href=""><img src="images/drums_t1.jpg"/></a>
+							<h2>Piano</h2>						
+						</div>
+									
+					</div><!--/container-->																		
+				</ul><!--/tab1-->
+   			 
+   			 
+				<ul id="t2" class="hide">
+    			    <!--great things-->								
+    			</ul><!--/tab2-->
+    			        			 
+			        			
+    			<ul id="t2" class="hide">
+    			    <!--great things-->							
+				</ul><!--/tab3-->
+
+			</div> <!-- END List Wrap -->
+		</div> <!--/tabs-->
+		
+		<div class="bumpbump"></div>
+		<div class="bumpbump"></div>		
+	
+</body>
+	
+</html>
