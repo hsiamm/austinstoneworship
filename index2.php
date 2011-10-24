aed8cfe9c6a5e985114d1b52b7ed08534c1040a2
ASCC Developer <communications@austinstone.org>
2011-10-24 07:38:19 -0500

    Stream and download Live

--- index2.php
+++ index2.php
@@ -10,7 +10,6 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
     <link rel="stylesheet" href="/css/jquery.fancybox-1.3.4.css" type="text/css" /> 
     <link href='css/jplayer_style.css' rel='stylesheet' type='text/css' />
-    <link href='css/thickbox.css' rel='stylesheet' type='text/css' />
 	<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">	
 	
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script> 
@@ -21,7 +20,6 @@
 
     <script src='js/jquery.min.js' type='text/javascript'></script>
     <script src='js/scripts.js' type='text/javascript'></script>
-    <script src='js/thickbox-compressed.js' type='text/javascript'></script>
     <script src='js/jquery.jplayer.min.js' type='text/javascript' ></script>
     <script type="text/javascript" src="../js/jquery.fancybox-1.3.4.pack.js"></script>
     <script type="text/javascript" src="../js/jquery.fancybox-1.3.4.js"></script>
@@ -105,10 +103,25 @@
 						<div id="jquery_jplayer_1" class="jp-jplayer"></div>
 						
 						<div id="jp_container_1" class="jp-audio">
+						
 					        <div class="jp-type-single">
 				                <div class="jp-gui jp-interface">
+
+				                 <!--<div id="custom-tweet-button">
+					                <a onClick="popup('https://twitter.com/share?url=http%3A%2F%2Faustinstone.org%2Fsermons%2Fitem%2Fthe-temptation-of-christ&text=Check out The+Temptation+of+Christ+%23ascc')" target="_blank">
+							            <img src="images/twitter.png" />
+							        </a>
+							    </div>-->
+				                 <!--<div id="custom-fb-button">
+					                <a onclick="fbs_click('http%3A%2F%2Faustinstone.org%2Fsermons%2Fitem%2Fthe-temptation-of-christ')" target="_blank">
+							            <img src="images/fb.png" />
+							        </a>
+							    </div>/facebook-->							    
+
+							    
 				                        <h3>TRACK</h3>
 				                        <h2 class="song_title"><span class="dark_gold">01</span> &nbsp;/&nbsp; GOD UNDEFEATABLE</h2>
+									    
 				                        <div class="jp-progress">
 				                                <div class="jp-seek-bar">
 				                                        <div class="jp-play-bar"></div>
@@ -127,7 +140,9 @@
 				                                &ndash;
 				                                <div class="jp-duration"></div>
 				                        </div>
-				                        <h4 class="download"><a href="#TB_inline?height=155&width=300&inlineId=email_signup" class="thickbox">Free Download</a></h4>
+				                        <div style="clear:both;"></div>
+				                        <a class="pop" href="#data"><h4 class="download">Free Download</h4></a>
+				                        <div style="display:none"><div id="data"><iframe height="340" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="https://theaustinstone.wufoo.com/embed/s6x4k1/"><a href="https://theaustinstone.wufoo.com/forms/s6x4k1/" title="Free Download for GOD UNDEFEATABLE">Free Download</a></iframe></div></div>		                        
 				                </div>
 				
 				                <div class="jp-no-solution">
@@ -142,7 +157,7 @@
 						<div class="onecol"><h1>Theology &amp; Resources</h1></div>
 						
 						<div class="threecol top_border">
-							<a class="pop" href="images/theo_pop.png"><img alt="Chord Chart" src="images/theo_t1.png" /></a>
+							<a href="files/god_undefeatable_theology.pdf"><img alt="Chord Chart" src="images/theo_t1.png" /></a>
 							<h2>Song Theology</h2>
 						</div>
 						
@@ -158,7 +173,7 @@
 						
 						<div class="bumpbump">&nbsp;</div>
 						
-						<div class="onecol"><h1>Teaching Videos</h1></div>
+						<!--<div class="onecol"><h1>Teaching Videos</h1></div>
 						
 						<div class="fourcol vid1_t1 top_border">
 							<a class="iframe" href="http://player.vimeo.com/video/29157489"><img src="images/story_t1.jpg"/></a>
@@ -178,7 +193,7 @@
 						<div class="fourcol vid4_t1 top_border">
 							<a class="iframe" href="http://player.vimeo.com/video/29157489"><img src="images/drums_t1.jpg"/></a>
 							<h2>Piano</h2>						
-						</div>
+						</div>-->
 									
 					</div><!--/container-->																		
 				</ul><!--/tab1-->
@@ -186,17 +201,27 @@
    			 	<div style="clear:both;"></div>
 				
 				<ul id="t2" class="hide">
-    			    <h1 class="notyet" style="text-align:center;">Whoa there, Track 02 doesn't release until November 14.<br>Be sure to check back then! </h1>							
+    			    <p style="text-align:center;height:400px;">Whoa there, Track 02 doesn't release until November 14.<br>Be sure to check back then! </h1>							
     			</ul><!--/tab2-->
 
    			 	<div style="clear:both;"></div>
 				
 				<ul id="t3" class="hide">
-    			    <h1 class="notyet" style="text-align:center;">Easy tiger, Track 03 doesn't release until November 28.<br>Be sure to check back then! </h1>							
+    			    <p style="text-align:center;height:400px;">Easy tiger, Track 03 doesn't release until November 28.<br>Be sure to check back then! </h1>							
     			</ul><!--/tab3-->    			
     			        			 
 			</div> <!-- END List Wrap -->
 		</div> <!--/tabs-->
+	</div><!--/container-->
+	
+	<div style="clear:both;"></div>
+	
+	<div class="container">
+	<div class="bump">&nbsp;</div>
+	<div class="top_border">&nbsp;</div>
+		<div class="social">
+			<a href="http://facebook.com/austinstoneworship"><p>Facebook</a> &nbsp;|&nbsp; <a href="http://twitter.com/asworship">Twitter</a> &nbsp;|&nbsp; <a href="mailto:info@austinstoneworship.com">Email</a></p></a>			
+		</div>
 	</div><!--/container-->
 
 	<div class="bumpbump"></div>
